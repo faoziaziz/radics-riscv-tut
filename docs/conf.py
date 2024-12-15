5bfa35fdc826f5535cdb8cb95458b8c858d63234
Aziz Faozi <aziz.faozi@radicstech.com>
2024-12-15 15:08:50 +0700

    lerem

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,6 @@ project = 'My Project'
 author = 'Aziz Amerul Faozi'
 version = '1.0'
 release = '1.0.0'
-html_theme = 'sphinx_book_theme'
+html_theme = 'sphinx_rtd_theme'
 extensions = ['sphinx.ext.autodoc']
 
